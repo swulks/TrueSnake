c044e3223630928de61c2d7c071215dbb5a01b15
swulks <swulyk@gmail.com>
2019-03-17 16:39:00 +0200

    Some changes from Linux2

--- TrueSnake/Program.cs
+++ TrueSnake/Program.cs
@@ -4,14 +4,8 @@
     {
         static void Main(string[] args)
         {
-            /*
             Game game = new Game();
             System.Console.WriteLine(0);
-            System.Console.ReadKey();
-            */
-
-            System.Console.WriteLine("Enter a number");
-            System.Console.WriteLine(System.Console.ReadLine());
             System.Console.ReadKey();
         }
     }
